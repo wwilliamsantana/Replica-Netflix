3171ab7597a7bde595fa314f27c76fb671e8170d
William Santana <84254929+wwilliamsantana@users.noreply.github.com>
2021-08-10 08:42:59 -0300

    Finalizado

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      400:{
+        items:2
+      },
+      600:{
+        items:3
+      },
+      800:{
+          items:4
+      },
+      900:{
+        items:5
+      },
+      1000:{
+          items:6
+      },
+      1200:{
+        items:7
+      }
+  }
+})
